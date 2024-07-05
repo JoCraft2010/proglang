ce811fd4cd5fa902ce286845071d1f9d7855a83a
Johannes Preiser <jocraft2010@gmail.com>
2024-07-05 19:49:07 +0200

    Added INFO macro to macros.h

--- compiler/src/macros.h
+++ compiler/src/macros.h
@@ -6,6 +6,10 @@
   #endif
 #endif
 
+#ifndef INFO
+  #define INFO(msg) std::cerr << "\033[1;34m" << msg << "\033[0m"
+#endif
+
 #ifndef ERROR
   #define ERROR(msg) std::cerr << "\033[1;31m" << msg << "\033[0m"
 #endif
